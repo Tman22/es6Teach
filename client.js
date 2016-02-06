0be4abf67daef522677ec9939ab893ba298b5015
Taylor Moore <Tmoore2272@gmail.com>
2016-02-06 14:59:59 -0700

    First lesson javascript

--- client.js
+++ client.js
@@ -5,6 +5,25 @@ $(document).ready( () => {
   wow()
 })
 
+
+class Demo {
+  constructor(param1, param2) {
+    this.p1 = param1 || "banana"
+    this.p2 = param2 || "cool stuff brah"
+  }
+
+  get outputParams() {
+    console.log(`${this.p1} AND ${this.p2}`)
+  }
+  outputStuff() {
+    this.outputParams
+  }
+}
+
+const shit = new Demo
+ shit.outputParams
+ shit.outputStuff()
+
 const wow = () => {
   return console.log('wow')
 }
